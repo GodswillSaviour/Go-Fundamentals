e487bdd84a41974562b3e0310575c6048e584e21
godswillsaviour <godswillsaviour89@gmail.com>
2026-06-04 00:18:29 +0100

    add: 01-hello-world - first Go program

---
+++ 01-hello-world/main.go
@@ -0,0 +1,20 @@
+// My first Go program
+// Topic: Variables & Output
+
+package main
+
+import "fmt"
+
+func main() {
+	// Declaring a variable to store my name
+	var name string = "Godswill"
+
+	// Printing hello world
+	fmt.Println("Hello, World!")
+
+	// Using the variable in output with Printf
+	fmt.Printf("My name is %s\n", name)
+
+	// Welcome message
+	fmt.Println("Welcome to my Go journey!")
+}
